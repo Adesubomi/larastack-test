7c3e2c6618449ac6ebee93b34565a9bf9bf31dce
Oniya Daniel <daniel.prince84@gmail.com>
2020-06-27 14:44:21 +0100

    vue project structured, first vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+import Vue from 'vue';
+import App from './components/App';
+
+Vue.component('app', App);
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="max-w-lg mx-auto mt-32">
+        <div>
+            <h1 class="text-xl">Larastack Testing</h1>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+
+            };
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
